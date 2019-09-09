113eb026e47893ed381d51f0c0837dede6900e14
Haroldf01 <haroldf0001@gmail.com>
2019-09-09 14:46:58 +0530

    Merged wing modal code in wing structure

--- src/Components/Forms/WingStructure.js
+++ src/Components/Forms/WingStructure.js
@@ -1,10 +1,132 @@
 import React from 'react';
-import SpringModal from './SpringModal';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { useSpring, animated } from 'react-spring';
+
+import { TextField, Button, Grid } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  }
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+function WingModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    console.log('Submited')
+    setOpen(false)
+  }
+
+  return (
+    <div>
+      <Button variant="outlined" color='secondary' onClick={handleOpen}>
+        Add Wings
+      </Button>
+      <Modal
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <Grid item lg={8} md={8} sm={10} xs={10}>
+              <TextField
+                required
+                id="wingName"
+                name="wingName"
+                label="Wing Name"
+                fullWidth
+                autoComplete="wName"
+              />
+            </Grid>
+
+            <Grid item lg={8} md={8} sm={10} xs={10}>
+              <TextField
+                required
+                id="numOfFloors"
+                name="numOfFloors"
+                label="Num of Floors"
+                fullWidth
+              />
+            </Grid>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={handleSubmit}
+              className={classes.button}
+            >
+              Done
+            </Button>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
 
 export default function WingStructure() {
   return (
       <div>
-          <SpringModal />
+          <WingModal />
       </div>
   );
 }
